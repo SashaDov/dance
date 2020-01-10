72a4dbeaeb23135af33300cca5d2bfa4bf7a286e
Galina <winter888@bk.ru>
2020-01-10 16:55:05 +0300

    Symphony project first. Initial commit.

---
+++ src/Controller/BarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BarController extends AbstractController
+{
+    /**
+     * @Route("/bar/index")
+     * @return Response
+     * @throws \Exception
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('bar/index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
